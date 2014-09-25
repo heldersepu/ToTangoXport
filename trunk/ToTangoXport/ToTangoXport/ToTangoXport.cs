a9dcaf6ab9b516d590f9b0d56c91254137754957
heldersepu <heldersepu@f9fbc5f1-cee6-fc76-ca60-fd1e815b03a3>
2014-09-25 16:05:48 +0000

    Clean up some unused items

--- trunk/ToTangoXport/ToTangoXport/ToTangoXport.cs
+++ trunk/ToTangoXport/ToTangoXport/ToTangoXport.cs
@@ -2,10 +2,6 @@
 using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using System.Configuration;
 using System.Runtime.InteropServices;
